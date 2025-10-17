6e05c04cd9a7e6f0011e72e4b8f54c3f1cf22eb9
Danielle Navarro <djnavarro@protonmail.com>
2025-10-17 23:12:25 +1100

    fix typo

--- tests/testthat/test-api.R
+++ tests/testthat/test-api.R
@@ -1,4 +1,4 @@
-test_that("emax_nls works withtest data", {
+test_that("emax_nls works with test data", {
   expect_no_error(emax_nls(
     structural_model = response_1 ~ exposure_1, 
     covariate_model = list(E0 ~ cnt_a, Emax ~ 1, logEC50 ~ 1), 
