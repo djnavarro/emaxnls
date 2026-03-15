12de53aac240ebf926ddca6f8486b60da5feeeed
Danielle Navarro <djnavarro@protonmail.com>
2026-03-15 14:21:54 +1100

    improves error messages in validators

--- R/utils-validators.R
+++ R/utils-validators.R
@@ -1,46 +1,49 @@
 
 .validate_structural_formula <- function(formula, names = NULL) {
 
-  # check that it is a two-sided formula with one variable on each side
-  .assert(inherits(formula, "formula"))
-  .assert(length(formula) == 3)
-  .assert(length(all.vars(formula)) == 2)
+  errmsg <- "`structural_formula` must be a two-sided formula of the form `response ~ exposure`"
+  .assert(inherits(formula, "formula"), errmsg)
+  .assert(length(formula) == 3, errmsg)
+  .assert(length(all.vars(formula)) == 2, errmsg)
 
-  # check that the variables refer to known variable names
-  if (!is.null(names)) .assert(all(all.vars(formula) %in% names))
+  errmsg <- "variables in `structural_formula` must exist in the data"
+  if (!is.null(names)) .assert(all(all.vars(formula) %in% names), errmsg)
 
 }
 
 .validate_covariate_formula <- function(formula, names = NULL) {
 
-  # check that "formula" is a list of 3 or 4 two-sided formulas
-  .assert(inherits(formula, "list"))
-  .assert(length(formula) %in% 3:4)
-  .assert(all(.map_lgl(formula, function(x) .is_formula(x, sides = 2))))
+  errmsg <- "`covariate_formula` must be a list of 3 or 4 two-sided formulas"
+  .assert(inherits(formula, "list"), errmsg)
+  .assert(length(formula) %in% 3:4, errmsg)
+  .assert(all(.map_lgl(formula, function(x) .is_formula(x, sides = 2))), errmsg)
 
-  # check that the LHS names correspond to the structural parameters
+  errmsg <- "variables on the left of `covariate_formula` must refer to structural parameters (E0, Emax, logEC50, logHill)"
   lhs_names <- unname(sort(.map_chr(formula, function(x) all.vars(x[[2]]))))
-  if (length(lhs_names) == 3) .assert(lhs_names == c("E0", "Emax", "logEC50"))
-  if (length(lhs_names) == 4) .assert(lhs_names == c("E0", "Emax", "logEC50", "logHill"))
+  if (length(lhs_names) == 3) .assert(lhs_names == c("E0", "Emax", "logEC50"), errmsg)
+  if (length(lhs_names) == 4) .assert(lhs_names == c("E0", "Emax", "logEC50", "logHill"), errmsg)
 
-  # check that RHS names refer to known variables
   if (!is.null(names)) {
+    errmsg <- "variables on the right of `covariate_formula` must exist in the data"
     rhs_names <- unique(unlist(.map(
       .x = formula, 
       .f = function(x) all.vars(x[[3]])
     )))
-    .assert(all(rhs_names %in% names))
+    .assert(all(rhs_names %in% names), errmsg)
   }
 }
 
 .validate_candidate_list <- function(candidates, names) {
 
-  .assert(inherits(candidates, "list"))
-  .assert(length(candidates) <= 4)
-  .assert(all(names(candidates) %in% c("E0", "Emax", "logEC50", "logHill")))
-  .assert(length(unique(names(candidates))) == length(names(candidates)))
-  .assert(all(.map_lgl(candidates, is.character)))
-  .assert(all(unlist(candidates) %in% names))
+  errmsg <- "`candidates` must be a named list, with unique names in c('E0', 'Emax', 'logEC50', 'logHill')"
+  .assert(inherits(candidates, "list"), errmsg)
+  .assert(length(candidates) <= 4, errmsg)
+  .assert(all(names(candidates) %in% c("E0", "Emax", "logEC50", "logHill")), errmsg)
+  .assert(length(unique(names(candidates))) == length(names(candidates)), errmsg)
+
+  errmsg <- "variables listed in `candidates` must exist in the data"
+  .assert(all(.map_lgl(candidates, is.character)), errmsg)
+  .assert(all(unlist(candidates) %in% names), errmsg)
 
 }
 
