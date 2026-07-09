1806048b7c859f6d877091f5a5762960094a4ad9
Danielle Navarro <djnavarro@protonmail.com>
2026-07-09 20:00:06 +1000

    softens emax_df test to allow tibble to data frame comparison

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -6,5 +6,9 @@ test_that("emax_df matches internal function", {
     error = function(e) NULL
   )
   skip_if(is.null(sim_df), "Skip when .simulate_emax_data fails")
-  expect_equal(emax_df, sim_df)
+  expect_true(inherits(sim_df, "data.frame"))
+  expect_equal(names(emax_df), names(sim_df))
+  for (nm in names(emax_df)) {
+    expect_equal(emax_df[[nm]], sim_df[[nm]])
+  }
 })
